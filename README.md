9a00f30ef03fccebf17ba673204e463e796445cd
Jacob Card-Howe <jhowe@howesites.com>
2021-10-21 09:22:17 -0400

    Initial commit pt.2 electric bugaloo
    Updated the minecraft service account username
    
    Adding another change directory
    
    Added some logging, fixed an issue where java wouldn't install
    
    Added in wget for Minecraft server
    
    Adding in eula for convenience
    
    Updated order of operations for granting Minecraft user access to executable shell scripts
    
    Updated to add logging instead of echoing
    
    Updated README and some comments
    
    Found my original check_users script, retro fitting it towards our new stuff rn
    
    Update start_minecraft.sh, updated userdata.sh
    
    Updated java heap sizes
    
    Added in server.properties, updated check_users .players directory
    
    whole buncha changes
    
    Changed up things to make ubuntu the owner since there seems to be some weird permissions issues going on
    
    FINALLY GOT THINGS WORKING
    
    Updated start_minecraft.sh to get rid of need for check_users.sh
    
    Removed check_users.sh from userdata
    
    Tweaked logic surrounding players interrupting shutdown sequence, replaced echos with loggers
    
    Fixed placement of shutdown
    
    More tweaks
    
    Some more tweaks to start_minecraft.sh
    
    Added in .start_minecraft file so that line 16 doesn't bomb out
    
    Removed echos and replaced with loggers
    
    Added some more logging
    
    Added in another area where we'll check that users are online
    
    Updated some logging messages, fixed some sleeps, GOT THE DAMN THING WORKING
    
    Added missing ampersands?
    
    Updated README to include instructions on how to use userdata.sh in AWS
    
    Removed check_users.sh in favor of a while loop in start_minecraft.sh
    
    Tweaked some settings in server.properties
    
    Fixed some sleep settings, fixed some log messages, some more general tweaks
    
    LET'S TRY THIS AGAIN
    
    please just fucking work you stupid fucking script
    
    Messed up a sleep setting
    
    Got rid of hacky, raw shell scripts and replaced them with generated ones via our userdata script. Additionally made minecraft / player_check run as services instead of individual shell scripts
    
    Deleted start_minecraft.sh
    
    README updates

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # mc-server
 A place for me to keep my scripts relating to my Minecraft server
+
+## General Description
+About once a year or so, my friends and I get an itch to play some Minecraft together. This repository serves as a script dump for the server.
+
+## What's in here?
+
+`eula.txt`: Accepts Mojang's [end user license agreement](https://account.mojang.com/documents/minecraft_eula).
+
+`server.properties`: The properties I've set for my friends' and I's Minecraft server. Check out [this Minecraft Wiki on `server.properties`](https://minecraft.fandom.com/wiki/Server.properties) for more information on what each argument changes.
+
+`README.md`: This :)
+
+`userdata.sh`: IT DOES EVERYTHING. It creates the `minecraft` service, `player_check` service, assigns appropriate permissions, and more! It's pretty well commented, so check it out.
+
+## How can I use this?
+
+If you have an AWS account, you can follow the steps below to get this working for you:
+
+1. Sign into the AWS console and navigate to EC2
+1. Click `Launch Instances` in the top left corner of your screen
+1. Configure your EC2 instance to your liking (make sure it's in a public subnet and has a public IP address or you'll be unable to access your Minecraft server!)
+1. On `Step 3: Configure Instance`, once you're done configuring everything to your liking, at the bottom you'll see a field for `User data`. Select the `As file` radio button, and upload `userdata.sh`.
+1. Configure the rest of your server to your liking with regards to storage, tags, and security groups.
+1. Launch your server and enjoy! :)
---
+++ userdata.sh
@@ -0,0 +1,169 @@
+#!/bin/bash
+
+# Creates Minecraft service user account
+sudo adduser --disabled-password --gecos 'User for running and managing Minecraft servers' minecraft
+sudo echo "minecraft     ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+
+sudo apt update # Updates apt repository
+sudo apt install openjdk-16-jre-headless -y # Installs java
+
+# Tests that Java was installed successfully and is executable
+if ! [ -x "$(command -v java)" ]; then
+    logger "Java was not installed, exiting."
+    exit 1
+else
+    logger "Java was installed successfully."
+fi
+
+# Change directories into newly created minecraft user's home directory
+cd /home/minecraft
+
+# Pulls in start_minecraft.sh and check_users.sh
+git clone https://github.com/jacob-howe/mc-server.git
+
+# Tests that mc-server was cloned successfully
+GIT_CLONE_CHECK=$(ls | wc -l)
+if [ $GIT_CLONE_CHECK -gt 0 ]; then
+    logger "Minecraft server files were cloned successfully."
+else
+    logger "Unable to clone Minecraft server files"
+    exit 1
+fi
+
+# Change directories into freshly pulled mc-server
+cd mc-server/
+
+# Downloads Minecraft Server 1.17.1
+wget https://launcher.mojang.com/v1/objects/a16d67e5807f57fc4e550299cf20226194497dc2/server.jar
+
+# Gives ownership of newly pulled directory to Minecraft user
+chown -R minecraft:minecraft .
+
+chmod a+x *.sh
+
+# Creates service for Minecraft Server
+sudo echo '[Unit]
+Description=A dedicated Minecraft server
+Wants=network.target
+After=local-fs.target network.target
+
+[Service]
+User=minecraft
+Group=minecraft
+UMask=0027
+
+# EnvironmentFile=/etc/conf.d/minecraft
+KillMode=none
+SuccessExitStatus=0 1 255
+
+ExecStart=/home/minecraft/start_minecraft.sh
+# ExecStop=/usr/bin/mcrcon -H localhost -p ${RCON_PASSWD} stop
+
+[Install]
+WantedBy=multi-user.target' > /etc/systemd/system/minecraft.service
+
+# Setting perms on minecraft.service
+sudo chmod 644 /etc/systemd/system/minecraft.service
+
+# Creates start up script for Minecraft Server Service
+sudo echo '#!/bin/bash
+cd /home/minecraft/mc-server
+java -Xms1024M -Xmx3072M -XX:ParallelGCThreads=1 -jar server.jar nogui
+' > /home/minecraft/start_minecraft.sh
+
+# Creates service for our Player Check
+sudo echo '[Unit]
+Description=Checks for online players
+Wants=network.target
+After=local-fs.target network.target minecraft.service
+
+[Service]
+User=minecraft
+Group=minecraft
+UMask=0027
+
+KillMode=none
+SuccessExitStatus=0 1 255
+
+ExecStart=/home/minecraft/player_check.sh
+
+[Install]
+WantedBy=multi-user.target' > /etc/systemd/system/player_check.service
+
+# Setting perms on player_check.service
+sudo chmod 644 /etc/systemd/system/minecraft.service
+
+sudo echo '#!/bin/bash
+
+set -e
+
+cd /home/minecraft
+touch .online_users
+touch .start_minecraft
+
+sudo chown -R minecraft:minecraft .
+
+while true; do
+    logger "Player check loop has begun, or started anew!"
+
+    logger "Calculating current number of online users via lsof on port 25565, sleeping for 5 seconds"
+    lsof -iTCP:25565 -sTCP:ESTABLISHED > .online_users &
+    sleep 5
+
+    logger "Setting current number of online users online by reading .online_users"
+    NUMLINES=$(wc -l < .online_users)
+    NUMUSERS=$(($NUMLINES - 1))
+
+    logger "Setting max timeout to be 120 seconds"
+    TIMEOUTMAX=$((SECONDS+120))
+
+    if [ $NUMUSERS -gt 0 ]
+    then
+        logger "There are $NUMUSERS online right now! Checking again in 30 seconds..."
+        sleep 30
+    else
+        logger "There are $NUMUSERS online right now, shutting down in 120 seconds unless someone comes online"
+        while [ $SECONDS -lt $TIMEOUTMAX ] && [ $NUMUSERS -lt 1 ] ;
+        do
+            logger "Checking to see if anyone has come online..."
+            lsof -iTCP:25565 -sTCP:ESTABLISHED > .online_users &
+            sleep 5
+            NUMUSERS=$(wc -l < .online_users)
+            if [ $NUMUSERS -gt 0 ]
+            then
+                logger "Someone came online. There are $NUMUSERS players online right now."
+                continue 2
+            fi
+        done
+        break
+    fi
+done
+
+
+logger "Stopping Minecraft..."
+killall java
+sleep 30
+
+if pgrep -x java >/dev/null
+then
+    logger "There was an issue while killing the java service"
+else
+    logger "Minecraft server stopped successfully"
+fi
+
+logger "Shutting down Server in 1 minute..."
+sudo shutdown' > /home/minecraft/player_check.sh
+
+# Setting perms on player_check.service
+sudo chmod 644 /etc/systemd/system/player_check.service
+
+# Enable both services so that they run on reboot
+sudo systemctl enable minecraft
+sudo systemctl enable player_check
+
+# Makes shell scripts executable in mc-server/
+chmod a+x /home/minecraft/*.sh
+
+# Start the services
+sudo systemctl start minecraft
+sudo systemctl start player_check
