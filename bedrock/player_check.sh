ef92b0e6e48ede48030c705912eebd5206e70ccd
Jacob Card-Howe <44793487+jacob-card-howe@users.noreply.github.com>
2024-07-26 08:59:50 -0400

    Tweaking min and max packet count values

--- bedrock/player_check.sh
+++ bedrock/player_check.sh
@@ -25,20 +25,20 @@ while true; do
     logger "Setting max timeout to be 120 seconds"
     TIMEOUTMAX=$((SECONDS+120))
 
-    if [ $NUMPACKETS -gt 1 ]
+    if [ $NUMPACKETS -gt 5 ]
     then
         logger "There were $NUMPACKETS sent in the last 5 seconds! Checking traffic again in 30 seconds..."
         sleep 30
     else
         logger "There are $NUMPACKETS sent recently, shutting down in 120 seconds unless someone comes online."
-        while [ $SECONDS -lt $TIMEOUTMAX ] && [ $NUMPACKETS -lt 1 ] ;
+        while [ $SECONDS -lt $TIMEOUTMAX ] && [ $NUMPACKETS -lt 10 ] ;
         do
             logger "Checking to see if anyone has come online..."
             sudo timeout "5" tcpdump -i any 'udp port 19132 and dst port 19132' > .tcp_dump &
             sleep 5
             NUMLINES=$(wc -l < .tcp_dump)
             NUMPACKETS=$(($NUMLINES - 1))
-            if [ $NUMPACKETS -gt 0 ]
+            if [ $NUMPACKETS -gt 5 ]
             then
                 logger "Someone came online. There were $NUMPACKETS sent recently."
                 continue 2
