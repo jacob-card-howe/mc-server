cd671d61bcc8dc996c831934ecd8241921048a2c
Jacob Card-Howe <44793487+jacob-card-howe@users.noreply.github.com>
2024-07-26 09:07:43 -0400

    Updating the start and stop scripts, continuing to tweak player_check

--- bedrock/player_check.sh
+++ bedrock/player_check.sh
@@ -4,19 +4,18 @@ set -e
 
 cd /home/minecraft/bedrock_server
 touch .tcp_dump
-touch .start_minecraft
 
-sudo chown -R minecraft:minecraft .
+sudo chown -R minecraft:minecraft
 
-logger "Initializing player_check.sh..."
+logger "Initializing player_check.sh and allowing Minecraft Bedrock server to start..."
 sleep 120
 
 while true; do
     logger "Player check loop has begun, or started anew!"
 
-    logger "Calculating current server network traffic on port 19132 via tcpdump, sleeping for 5 seconds"
+    logger "Calculating current server network traffic on port 19132 via tcpdump, sleeping for 10 seconds"
     sudo timeout "5" tcpdump -i any 'udp port 19132 and dst port 19132' > .tcp_dump &
-    sleep 5
+    sleep 10
 
     logger "Setting number of recently sent packets by reading .tcp_dump"
     NUMLINES=$(wc -l < .tcp_dump)
@@ -25,17 +24,16 @@ while true; do
     logger "Setting max timeout to be 120 seconds"
     TIMEOUTMAX=$((SECONDS+120))
 
-    if [ $NUMPACKETS -gt 5 ]
-    then
-        logger "There were $NUMPACKETS sent in the last 5 seconds! Checking traffic again in 30 seconds..."
+    if [ $NUMPACKETS -gt 5 ]; then
+        logger "There were $NUMPACKETS sent in the last 5 seconds! Checking for traffic again in 30 seconds..."
         sleep 30
     else
-        logger "There are $NUMPACKETS sent recently, shutting down in 120 seconds unless someone comes online."
+        logger "There were $NUMPACKETS sent recently, shutting down in 120 seconds unless someone comes online."
         while [ $SECONDS -lt $TIMEOUTMAX ] && [ $NUMPACKETS -lt 10 ] ;
         do
             logger "Checking to see if anyone has come online..."
             sudo timeout "5" tcpdump -i any 'udp port 19132 and dst port 19132' > .tcp_dump &
-            sleep 5
+            sleep 10
             NUMLINES=$(wc -l < .tcp_dump)
             NUMPACKETS=$(($NUMLINES - 1))
             if [ $NUMPACKETS -gt 5 ]
--- bedrock/start_server.sh
+++ bedrock/start_server.sh
@@ -2,9 +2,9 @@
 
 SERVER_PATH=/home/minecraft/bedrock_server/
 
-if [ -d $SERVER_PATH/worlds ]; then 
+if [ -d $SERVER_PATH/worlds ]; then
     logger "Backing up world..."
-    /usr/bin/gcloud storage cp -r /home/minecraft/bedrock_server/worlds gs://jch-minecraft-world-backups/bedrock/$(date +"%m-%d-%Y-%H:%M:%S")/worlds
+    /usr/bin/gcloud storage cp -r /home/minecraft/bedrock_server/worlds gs://jch-minecraft-world-backups/bedrock/$(date +"%m-%d-%Y-%H:%M:%S")-session-start/worlds
 fi
 
 /usr/bin/screen -dmS minecraft_bedrock /bin/bash -c "LD_LIBRARY_PATH=$SERVER_PATH ${SERVER_PATH}bedrock_server"
