e5249784d6c4675a552cd4419b27d7a83d38a6bf
Jacob Card-Howe <44793487+jacob-card-howe@users.noreply.github.com>
2024-07-30 07:46:21 -0400

    Updating GCS path

--- bedrock/stop_server.sh
+++ bedrock/stop_server.sh
@@ -9,6 +9,6 @@ MINECRAFT_BEDROCK_DIR=$MINECRAFT_HOME_DIR/bedrock_server
 
 WORLD_BACKUPS_BUCKET="gs://jch-minecraft-world-backups/bedrock"
 
-logger "Backing up world to $WORLD_BACKUPS_BUCKET/$(date +"%m-%d-%Y-%H:%M:%S")/worlds..."
+logger "Backing up world to $WORLD_BACKUPS_BUCKET/$(date +"%m-%d-%Y")/session-end/worlds..."
 
-/usr/bin/gcloud storage cp -r /home/minecraft/bedrock_server/worlds gs://jch-minecraft-world-backups/bedrock/$(date +"%m-%d-%Y-%H:%M:%S")-session-end/worlds
+/usr/bin/gcloud storage cp -r /home/minecraft/bedrock_server/worlds gs://jch-minecraft-world-backups/bedrock/$(date +"%m-%d-%Y")/session-end/worlds
